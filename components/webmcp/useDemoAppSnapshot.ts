85a49f6dfb6e880139fd00052b04e058e05e1b3a
Durgesh <durgesh@newtuple.com>
2026-09-02 15:34:41 +0530

    refactor: remove legacy demo app generation system and introduce product-focused navigation and recommendations

--- components/webmcp/useDemoAppSnapshot.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { useSyncExternalStore } from 'react'
-import { demoAppStore } from '@/lib/demoApp/store'
-
-export const useDemoSnapshot = () =>
-  useSyncExternalStore(demoAppStore.subscribe, demoAppStore.getSnapshot, demoAppStore.getServerSnapshot)
